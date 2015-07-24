1f9231250009323e9a7fb849c6c6dfb608022ee1
Mollie Stephenson <mollie.c.stephenson@gmail.com>
2015-07-24 20:11:04 +0100

    comment test passing

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -4,12 +4,14 @@ require_relative 'helpers.rb'
 feature 'comments' do
   context 'from the homepage' do
     scenario 'adding a comment' do
-      sign_up
       Photo.create(title: 'Mountains', image_file_name: 'mountains.jpg')
+      sign_up
+      create_profile
       visit '/'
       click_link 'Mountains'
       fill_in 'Text', with: 'Nice photo'
       click_button 'Create Comment'
+      expect(page).to have_content 'Nice photo'
     end
   end
 end
